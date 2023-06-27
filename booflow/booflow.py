568b262462ef2906303114078c2377fe5e8b4570
weijay <weijay0804@gmail.com>
2023-06-27 19:09:02 +0800

    refactor: 將 `TaskManager` 部分功能交給 `Task` 管理
    把 `TaskManager` 類別的部分功能切分出來，讓 `Task` 類別處理

--- booflow/booflow.py
+++ booflow/booflow.py
@@ -60,7 +60,7 @@ class Cron:
         """將指令字串解析成 list
 
         Args:
-            cmd (str): 指令字串，例如 "python3 main.py"
+            cmd (str): 指令字串，例如 "python3 main.py".
 
         Returns:
             list: 回傳解析後的指令 => ["python3", "main.py"]
@@ -120,49 +120,243 @@ class Cron:
         return self.run()
 
 
-class TaskManager:
-    """任務管理"""
+class Task:
+    """任務佇列 (被 :class:`TaskManager` 使用)
 
-    def __init__(self, order):
-        task_graph, indegree = self.transform_tasks_to_graph(order)
-        self.order_list = self.extract_graph_task_order(task_graph, indegree)
+    通常會一個 :class:`Task` 會被一個 :class:`TaskManager` 管理，
+    :class:`Task` 類別只是方便被 :class:`TaskManager` 管理建立的。
 
-    def transform_tasks_to_graph(self, tasks: List[tuple]) -> Tuple[defaultdict, defaultdict]:
+    Args:
+        - tasks_orders (List[tuple]): 任務順序清單
+
+    Attribute:
+
+        - tasks_order_queue (dequeu): 任務執行佇列。
+
+        - next (str): 回傳一個在目前狀態可以執行的任務名稱 (也就是說這個任務的依賴任務已經完成)。
+
+        - is_empty (bool): 檢查目前的任務佇列是否為空的，如果是空的回傳 `True` ，反之回傳 `False`。
+
+        - success_task (set): 順利執行完成的任務集合。
+
+        - faile_task (defaultidict(set)): 執行失敗的任務字典 ( 其中 `key` 為失敗的任務名稱，而 `value` 為因為依賴這個任務而無法執行的任務集合) 。
+
+    Usage:
+        - 初始化物件時會自動產生任務有向圖等相關屬性。
+        - 使用 :meth:`Task.next` 取得一個目前狀態下可以開始執行的任務名。
+        - 使用 :meth:`Task.is_empty` 來檢查任務佇列是否為空。
+        - 使用 :meth:`Task.remove_faile_task()` 移除一個失敗的任務，如果有其他任務依賴這個任務，也會被移除，並將相關訊息記錄到 `faile_task` 中，並且會自動更新 `graph` 、 `tasks_order` 等。
+        - 使用 :meth:`Task.remove_success_task()` 移除一個成功的任務，並且將訊息記錄到 `success_tasks` 中，並且會自動更新 `graph` 、 `tasks_order` 等。
+    """
+
+    def __init__(self, tasks_order: List[tuple]):
+        self._tasks_order = tasks_order
+
+        # 獨立的任務佇列 ( 沒有依賴其他任務 )
+        self._independent_tasks_queue = deque()
+
+        # 先把 tasks_order 轉換成 graph
+        self._graph, self._indegree, self._reverse_dependencies_graph = self._tasks_order_to_graph(
+            self._tasks_order
+        )
+
+        self.tasks_order_queue = self._gen_tasks_queue(self._graph, self._indegree)
+        self.success_tasks = set()
+        self.faile_tasks = defaultdict(set)
+
+    def _tasks_order_to_graph(
+        self, tasks_order: List[tuple]
+    ) -> Tuple[defaultdict, defaultdict, defaultdict]:
+        """將任務順序列表轉換成有向圖，前兩個回傳的值可以給 :meth:`Task._gen_tasks_queue` 當參數傳入
+
+        Args:
+            tasks_order (List[tuple]): 任務順序清單
+
+        Returns:
+            Tuple[defaultdict, defaultdict, defaultdict]: ( 有向圖資料結構 (`graph`), 入度字典 (`indegree`), 反向關聯有向圖 (`reverse_dependencies_graph`) )
+        """
         graph = defaultdict(set)
         indegree = defaultdict(int)
-        for start, end in tasks:
+        # 加入反向關聯
+        reverse_dependencies = defaultdict(set)
+
+        for start, end in tasks_order:
             if end not in graph[start]:
                 graph[start].add(end)
                 indegree[start]
                 indegree[end] += 1
+                reverse_dependencies[end].add(start)
 
-        return graph, indegree
+        return graph, indegree, reverse_dependencies
 
-    def extract_graph_task_order(self, graph: defaultdict, indegree: defaultdict) -> deque:
-        task_order = deque()
+    def _gen_tasks_queue(self, graph: defaultdict, indegree: defaultdict) -> deque:
+        """生成 tasks 執行順序佇列，在使用這個方法前，應該執行 :meth:`Task._tasks_order_to_graph` 方法
+
+        Args:
+            graph (defaultdict): 經由 :meth:`Task._tasks_order_to_graph` 生成的 graph.
+
+            indegree (defaultdict): 經由 :meth:`Task._tasks_order_to_graph` 生成的 indegree.
+
+        Returns:
+            deque: 任務執行順序佇列
+        """
+
+        task_queue = deque()
         queue = deque()
+
         for idx, item in indegree.items():
             if item == 0:
                 queue.append(idx)
 
         while queue:
             current_task = queue.popleft()
-            task_order.append(current_task)
+            task_queue.append(current_task)
             for next_task in graph[current_task]:
                 indegree[next_task] -= 1
                 if indegree[next_task] == 0:
                     queue.append(next_task)
 
-        return task_order
+        return task_queue
 
-    def get_next(self):
-        return self.order_list.popleft()
+    def _remove_faile_task(self, graph: defaultdict, target: str, root_target: str):
+        """輔助 :meth:`Task.remove_faile_task` 方法，使用遞迴的方式刪除跟 `target` 相關的節點，並將刪除的任務加入 `faile_tasks`
 
-    def is_empty(self):
-        return len(self.order_list) == 0
+        Args:
+            graph (defaultdict): 要從哪個 graph 中刪除節點
 
-    def call(self, task_name, status):
-        pass
+            target (str): 要刪除的任務名 (節點)
+        """
+
+        if graph.get(target):
+            target_items = graph[target]
+
+            del graph[target]
+
+            for item in target_items:
+                self.faile_tasks[root_target].add(item)
+                self._remove_faile_task(graph, item, root_target=root_target)
+
+    def remove_faile_task(self, target):
+        """從 graph 中移除失敗的任務，如果有其他任務依賴這個任務，也會一起刪除"""
+
+        self.faile_tasks[target]
+
+        self._remove_faile_task(self._graph, target, root_target=target)
+
+    def remove_success_task(self, target: str):
+        """從 graph 中除成功的任務，並將任務加入至 `success_tasks`
+
+        Args:
+            target (str): 要移除的 task name
+        """
+
+        self.success_tasks.add(target)
+
+        # target 是獨立任務
+        if not self._graph.get(target):
+            return
+
+        del self._graph[target]
+
+        # 檢查任務中是否有獨立的任務，如果有就加入到 independent_tasks_queue 中
+        # 先使用反向關聯圖檢查依賴的任務
+        for end, start_set in self._reverse_dependencies_graph.items():
+            # 如果 start_set 長度是 1 就代表此時任務只依賴一個任務
+            # 如果又剛好依賴的任務是要刪除的任務，那麼該任務就有可能會變成獨立任務
+            if len(start_set) == 1 and target in start_set:
+                # 如果在 graph 中該任務不是一個 key 的話，就表示該任務是獨立任務
+                # 可以想成因為該節點沒有指向下一個節點
+                if not self._graph.get(end):
+                    self._independent_tasks_queue.append(end)
+
+    def _graph_to_tasks_order(self, graph: defaultdict) -> List[tuple]:
+        """將 graph 轉換成 tasks_orders
+
+        Args:
+            graph (defaultdict(set)): 要轉換的 graph
+
+        Returns:
+            List[tuple]: 將傳入的 `graph` 轉換後的任務順序清單
+
+        """
+
+        new_tasks = []
+        for start, end_set in graph.items():
+            for end in end_set:
+                new_tasks.append((start, end))
+
+        return new_tasks
+
+    def update_tasks_order_queue(self):
+        """在使用 `remove_success_task` 或 `remove_faile_task` 需要使用這個函示
+
+        會根據目前的 `self_graph` 重新生成新的 `tasks_order`，在由 `tasks_order` 重新生成 `graph` 等相關屬性
+        最後再重新生成新的 `tasks_order_queue`
+
+        """
+
+        # 如果使用了 `remove_success_task` 或 `remove_faile_task` 更新了 graph
+        # 要依序更新 graph -> tasks_orders -> graph -> tasks_order_queue
+        # 先把新的 graph 轉換成 tasks_order
+        self._tasks_order = self._graph_to_tasks_order(self._graph)
+
+        # 更新相關屬性
+        self._graph, self._indegree, self._reverse_dependencies_graph = self._tasks_order_to_graph(
+            self._tasks_order
+        )
+        self.tasks_order_queue = self._gen_tasks_queue(self._graph, self._indegree)
+
+    def is_task_can_be_execute(self, task_name: str) -> bool:
+        """判斷傳入的 task 在目前的狀態是否可以執行 (有可能依賴任務還沒完成)
+
+        Args:
+            task_name (str): 要判斷的任務名
+
+        Returns:
+            bool: 如果可以執行，回傳 `True` ，反之 `False`
+        """
+
+        if self._reverse_dependencies_graph.get(task_name):
+            for dependen_task in self._reverse_dependencies_graph.get(task_name):
+                if dependen_task not in self.success_tasks:
+                    return False
+
+        return True
+
+    @property
+    def next(self) -> str:
+        """取得在目前狀態下，可以開始執行的任務名稱 (也就是這個任務依賴的任務已經完成，或是這個任務是獨立的任務)
+
+        Returns:
+            str: 回傳一個任務名稱
+        """
+
+        if self.tasks_order_queue:
+            first_task = self.tasks_order_queue[0]
+
+            if self.is_task_can_be_execute(first_task):
+                return self.tasks_order_queue.popleft()
+
+        if self._independent_tasks_queue:
+            return self._independent_tasks_queue.popleft()
+
+        return None
+
+    @property
+    def is_empty(self) -> bool:
+        """檢查任務佇列或是獨立任務佇列是否為空的，可以用來檢查排程是否要繼續執行
+
+        Returns:
+            bool: 如果佇列中還有東西則會傳 `True`，反之回傳 `False`
+        """
+
+        return len(self.tasks_order_queue) == 0 and len(self._independent_tasks_queue) == 0
+
+
+class TaskManager:
+    """任務管理"""
+
+    pass
 
 
 class CronManager:
---
+++ tests/test_task.py
@@ -0,0 +1,240 @@
+'''
+Author: weijay
+Date: 2023-06-22 22:27:41
+LastEditors: weijay
+LastEditTime: 2023-06-27 19:04:50
+Description: Task 單元測試
+'''
+
+import unittest
+from collections import defaultdict, deque
+
+from booflow.booflow import Task
+
+
+class TestTask(unittest.TestCase):
+    def test_tasks_order_to_graph(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task([])
+
+        graph, indegree, reverse_depne_graph = task_obj._tasks_order_to_graph(test_tasks_order)
+
+        test_graph = defaultdict(set)
+        test_graph["A"].add("B")
+        test_graph["A"].add("C")
+        test_graph["B"].add("D")
+
+        test_indegree = defaultdict(int)
+        test_indegree["A"] = 0
+        test_indegree["B"] = 1
+        test_indegree["C"] = 1
+        test_indegree["D"] = 1
+
+        test_rever_depen_graph = defaultdict(set)
+        test_rever_depen_graph["B"].add("A")
+        test_rever_depen_graph["C"].add("A")
+        test_rever_depen_graph["D"].add("B")
+
+        self.assertEqual(graph, test_graph)
+        self.assertEqual(indegree, test_indegree)
+        self.assertEqual(reverse_depne_graph, test_rever_depen_graph)
+
+    def test_gen_tasks_queue(self):
+        task_obj = Task([])
+
+        test_graph = defaultdict(set)
+        test_graph["A"].add("B")
+        test_graph["A"].add("C")
+        test_graph["B"].add("D")
+
+        test_indegree = defaultdict(int)
+        test_indegree["A"] = 0
+        test_indegree["B"] = 1
+        test_indegree["C"] = 1
+        test_indegree["D"] = 1
+
+        task_queue = task_obj._gen_tasks_queue(test_graph, test_indegree)
+
+        test_task_queue_list = []
+        test_task_queue_list.append(["A", "B", "C", "D"])
+        test_task_queue_list.append(["A", "C", "B", "D"])
+
+        self.assertIn(list(task_queue), test_task_queue_list)
+
+    def test_remove_faile_task(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        test_graph = defaultdict(set)
+        test_graph["A"].add("B")
+        test_graph["A"].add("C")
+        test_graph["B"].add("D")
+        test_graph["C"]
+        test_graph["D"]
+
+        self.assertTrue(len(task_obj.faile_tasks) == 0)
+        self.assertEqual(task_obj._graph, test_graph)
+
+        task_obj.remove_faile_task("A")
+
+        test_faile_tasks = defaultdict(set)
+        test_faile_tasks["A"].add("B")
+        test_faile_tasks["A"].add("C")
+        test_faile_tasks["A"].add("D")
+
+        self.assertEqual(task_obj.faile_tasks, test_faile_tasks)
+
+        test_graph = defaultdict(set)
+        test_graph["C"]
+        test_graph["D"]
+
+        self.assertEqual(task_obj._graph, test_graph)
+
+    def test_remove_success_task(self):
+        test_task_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_task_order)
+
+        test_graph = defaultdict(set)
+        test_graph["A"].add("B")
+        test_graph["A"].add("C")
+        test_graph["B"].add("D")
+        test_graph["C"]
+        test_graph["D"]
+
+        self.assertTrue(len(task_obj.faile_tasks) == 0)
+        self.assertEqual(task_obj._graph, test_graph)
+
+        task_obj.remove_success_task("A")
+
+        test_success_tasks = set()
+        test_success_tasks.add("A")
+
+        self.assertEqual(test_success_tasks, task_obj.success_tasks)
+
+        test_graph = defaultdict(set)
+        test_graph["B"].add("D")
+        test_graph["D"]
+        test_graph["C"]
+
+        test_indepndent_tasks = deque()
+        test_indepndent_tasks.append("C")
+
+        self.assertEqual(task_obj._graph, test_graph)
+        self.assertEqual(task_obj._independent_tasks_queue, test_indepndent_tasks)
+
+    def test_graph_to_tasks_order(self):
+        task_obj = Task([])
+
+        test_graph = defaultdict(set)
+        test_graph["A"].add("B")
+        test_graph["A"].add("C")
+        test_graph["B"].add("D")
+        test_graph["C"]
+        test_graph["D"]
+
+        test_tasks_order_queue_list = []
+        test_tasks_order_queue_list.append([("A", "B"), ("A", "C"), ("B", "D")])
+        test_tasks_order_queue_list.append([("A", "C"), ("A", "B"), ("B", "D")])
+
+        self.assertIn(task_obj._graph_to_tasks_order(test_graph), test_tasks_order_queue_list)
+
+    def test_update_tasks_order_queue(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        task_obj.remove_success_task("A")
+
+        test_tasks_order_queue_list = []
+        test_tasks_order_queue_list.append(deque(["A", "B", "C", "D"]))
+        test_tasks_order_queue_list.append(deque(["A", "C", "B", "D"]))
+
+        self.assertIn(task_obj.tasks_order_queue, test_tasks_order_queue_list)
+
+        task_obj.update_tasks_order_queue()
+
+        self.assertEqual(task_obj.tasks_order_queue, deque(["B", "D"]))
+
+    def test_is_task_can_be_execute(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        self.assertTrue(task_obj.is_task_can_be_execute("A"))
+        self.assertFalse(task_obj.is_task_can_be_execute("B"))
+        self.assertFalse(task_obj.is_task_can_be_execute("C"))
+        self.assertFalse(task_obj.is_task_can_be_execute("D"))
+
+        task_obj.remove_success_task("A")
+        task_obj.update_tasks_order_queue()
+
+        self.assertTrue(task_obj.is_task_can_be_execute("B"))
+        self.assertTrue(task_obj.is_task_can_be_execute("C"))
+        self.assertFalse(task_obj.is_task_can_be_execute("D"))
+
+    def test_next_with_remove_success_task(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        self.assertEqual(task_obj.next, "A")
+        self.assertIsNone(task_obj.next)
+
+        # 回報任務
+        task_obj.remove_success_task("A")
+        task_obj.update_tasks_order_queue()
+
+        self.assertEqual(task_obj.next, "B")
+        self.assertEqual(task_obj.next, "C")
+        self.assertIsNone(task_obj.next)
+
+    def test_next_with_remove_faile_task(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        self.assertEqual(task_obj.next, "A")
+        self.assertIsNone(task_obj.next)
+
+        # 回報任務
+        task_obj.remove_faile_task("A")
+        task_obj.update_tasks_order_queue()
+
+        self.assertIsNone(task_obj.next)
+
+    def test_is_empty_with_remove_success_task(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        self.assertFalse(task_obj.is_empty)
+
+        task_obj.remove_success_task("A")
+        task_obj.update_tasks_order_queue()
+
+        self.assertFalse(task_obj.is_empty)
+
+        task_obj.remove_success_task("B")
+        task_obj.update_tasks_order_queue()
+
+        self.assertFalse(task_obj.is_empty)
+
+        task_obj.remove_success_task("D")
+        task_obj.update_tasks_order_queue()
+
+        self.assertFalse(task_obj.is_empty)
+
+    def test_is_empty_with_remove_faile_task(self):
+        test_tasks_order = [("A", "B"), ("A", "C"), ("B", "D")]
+
+        task_obj = Task(test_tasks_order)
+
+        self.assertFalse(task_obj.is_empty)
+
+        task_obj.remove_faile_task("A")
+        task_obj.update_tasks_order_queue()
+
+        self.assertTrue(task_obj.is_empty)
